af25cdf7c47f529e3175c4774fc7f3a8425f7d53
Brandon-JP <98441280+Brandon-JP@users.noreply.github.com>
2023-05-16 10:26:23 -0400

    Add files via upload

--- LinguaLearn/LinguaLearn/mainForm.cs
+++ LinguaLearn/LinguaLearn/mainForm.cs
@@ -9,7 +9,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Runtime.CompilerServices;
 
 namespace LinguaLearn
 {
@@ -19,13 +18,10 @@ namespace LinguaLearn
         User currentUser;
         Record currentRecord;
         Timer timer = new Timer();
-        int lang = 0;
-        static List<Record> records;
         public mainForm(User currentUser)
         {
-            records = JsonConvert.DeserializeObject<List<Record>>(File.ReadAllText("..\\..\\recordsJSON"));
             this.currentUser = currentUser;
-            currentRecord = LoadRecord(currentUser.Username);
+           currentRecord = LoadRecord(currentUser.Username);
             InitializeComponent();
             // Set up the timer
             timer.Interval = 1000; // 1 second
@@ -43,25 +39,11 @@ namespace LinguaLearn
         {
             string json = File.ReadAllText("..\\..\\recordsJSON");
             List<Record> records = JsonConvert.DeserializeObject<List<Record>>(json);
-            Record record = null;
-            if (records != null)
-            {
-                record = records.FirstOrDefault(u => u.Username == username);
-            }
-            else { 
-                records = new List<Record>();
-            }
-            
-            
+            Record record = records.FirstOrDefault(u => u.Username == username);
             if (record == null)
             {
                 MessageBox.Show($"No record was found for user '{username}'.");
-                record = new Record(username);
-                record.Exercises = new List<string>();
-                records.Add(record);
-                SaveRecords(records);
-                return record;
-
+                return null;
             }
             else
             {
@@ -69,63 +51,6 @@ namespace LinguaLearn
             }
         }
 
-        public void UpdateRecord() {
-            if (records == null) {
-                records = new List<Record>();
-            }
-            foreach (Record record in records) {
-                if (record.Username == this.currentUser.Username) {
-                    record.Exercises = currentRecord.Exercises;
-                }
-            }
-            SaveRecords(records);
-        }
-
-        public static void SaveRecords(List<Record> records)
-        {
-            string json = JsonConvert.SerializeObject(records);
-            File.WriteAllText("..\\..\\recordsJSON", json);
-
-        }
-
-        private void grammerButton_Click(object sender, EventArgs e)
-        {
-            GrammerForm newGrammer = new GrammerForm(currentRecord, this, lang);
-            newGrammer.Show();
-            this.Hide();
-        }
-
-        private void vocabularyButton_Click(object sender, EventArgs e)
-        {
-            DictationForm newDictation = new DictationForm(currentRecord, this);
-            newDictation.Show();
-            this.Hide();
-        }
-
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            UpdateRecord();
-            Application.Exit();
-        }
-
-        private void recordButton_Click(object sender, EventArgs e)
-        {
-            string recordString = "";
-            foreach (string exercise in currentRecord.Exercises) { 
-                recordString += exercise +"\n";
-            }
-            MessageBox.Show(recordString, $"{currentUser.Username}'s Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-
-        private void languageComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            lang = languageComboBox.SelectedIndex;
-        }
-
-        private void aboutButton_Click(object sender, EventArgs e)
-        {
-            aboutForm aboutForm = new aboutForm();
-            aboutForm.Show();
-        }
+      
     }
 }
